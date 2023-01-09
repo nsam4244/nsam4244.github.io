e2391e5d0beaee0aa38fde4e503e5ae4274aba67
nsam4244 <n.sam@ardencahillacademy.com>
2023-01-09 09:37:26 +0000

    install projects

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -27,10 +27,97 @@ var level01 = function (window) {
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
+        function createSawBlade(x, y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+            game.addGameItem(sawBladeHitZone);
+            var obstacleImage = draw.bitmap('img/sawblade.png');
+            sawBladeHitZone.addChild(obstacleImage);
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }
+        createSawBlade(500, 330);
+        createSawBlade(800, 220);
+        createSawBlade(1200, 330);
 
+        function createOran(x, y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var raccoonHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            raccoonHitZone.x = x;
+            raccoonHitZone.y = y;
+            game.addGameItem(raccoonHitZone);
+            var obstacleImage = draw.bitmap('img/oran.png');
+            raccoonHitZone.addChild(obstacleImage);
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }
+        createOran(1600, 325);
+
+        function createEnemy(x, y) {
+            var enemy = game.createGameItem('enemy', 50);
+            var redSquare = draw.bitmap('img/thorn.png');
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redSquare);
+            enemy.x = x;
+            enemy.y = y;
+            enemy.velocityX = -1;
+            game.addGameItem(enemy);
+
+
+            enemy.onPlayerCollision = function () {
+                console.log('IVE BEEN SHOT');
+                game.changeIntegrity(-10);
+            };
+            enemy.onProjectileCollision = function () {
+                console.log('PLAYER DOWN');
+                game.increaseScore(1);
+                enemy.shrink();
+
+            }
+        }
+        createEnemy(400, groundY - 36);
+        createEnemy(800, groundY - 45);
+        createEnemy(1200, groundY - 59);
         
-        
-        
+        function createReward(x, y) {
+            function reward(x, y) {
+                var reward = game.createGameItem('reward', 10);
+                var purp = draw.bitmap('img/purp.png');
+                purp.x = -50;
+                purp.y = -50;
+                reward.addChild(purp);
+
+                purp.scaleX = 0.2;
+                purp.scaleY = 0.2;
+
+                reward.x = x;
+                reward.y = y;
+
+                game.addGameItem(reward);
+                reward.velocityX = -2;
+
+                reward.onPlayerCollision = function () {
+                    console.log('You collected the rare RACCOON COIN!!');
+                    game.changeIntegrity(+25);
+                    reward.fadeOut();
+
+                };
+                reward.onProjectileCollision = function () {
+                    console.log('You collected the rare RACCOON COIN!!');
+                    game.increaseScore(100);
+                    reward.fadeOut();
+                }
+            }
+            reward(1600, groundY - 50);
+
+        }
+        createReward();
+
         // DO NOT EDIT CODE BELOW HERE
     }
 };
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,33 +1,33 @@
 var background = function (window) {
     'use strict';
-    
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
-    window.opspark.makeBackground = function(app,ground) {
+    window.opspark.makeBackground = function (app, ground) {
         /* Error Checking - DO NOT DELETE */
-        if(!app) {
+        if (!app) {
             throw new Error("Invalid app argument");
         }
-        if(!ground || typeof(ground.y) == 'undefined') {
+        if (!ground || typeof (ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
-        
-     
+
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,21 +36,49 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.bitmap('img/ballz.jpg');
             background.addChild(backgroundFill);
-            
+
+
             // TODO: 3 - Add a moon and starfield
             
-            
+            var moon = draw.bitmap('img/moon.png');
+            moon.x = 1000;
+            moon.y = 20;
+            moon.scaleX = .6;
+            moon.scaleY = .6;
+            background.addChild(moon);
+           
+            for(var i = 0; i < 100; i++ ) {
+            var circle = draw.circle(5, 'white', 'LightGray', 1);
+            circle.x = canvasWidth * Math.random();
+            circle.y = groundY * Math.random();
+            background.addChild(circle);
+            }
+
+
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+            for (var i = 0; i < 9; ++i) {
+                var buildingHeight = Math.random() * 400;
+                var building = draw.rect(75, buildingHeight, 'lightgray', 1);
+                building.x = 200 * i;
+                building.y = groundY - buildingHeight;
+                background.addChild(building);
+                buildings.push(building);
+            }
+
             // TODO 4: Part 1 - Add a tree
-            
-            
+            tree = draw.bitmap('img/tree.png');
+            tree.x = 200;
+            tree.y = 100;
+            background.addChild(tree);
+
+
         } // end of render function - DO NOT DELETE
-        
-        
+        var tree; 
+        var buildings = [];  
+
+
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -58,26 +86,37 @@ var background = function (window) {
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
+            tree.x = tree.x - 4;
+
+            if(tree.x < -200) {
+                tree.x = canvasWidth;
+            }
+            for (var i = 0; i < buildings.length; i++) {
+                buildings[i].x = buildings[i].x - 1; 
+                if(buildings[i].x < -200) {
+                    buildings[i].x = canvasWidth;
+                }
             
-            
+                // code to do something with each element
+            }
             // TODO 5: Part 2 - Parallax
-            
+
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
+
+
+
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
         background = new createjs.Container();
         background.resize = render;
         background.update = update;
-        
+
         /* make the background able to respond to resizing and timer updates*/
         app.addResizeable(background);
         app.addUpdateable(background);
-        
+
         /* render and return the background */
         render();
         return background;
@@ -85,7 +124,7 @@ var background = function (window) {
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = background;
