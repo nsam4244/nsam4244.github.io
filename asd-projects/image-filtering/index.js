10b92301619804990c63e8de43de5feeeb81f07d
nsam4244 <n.sam@ardencahillacademy.com>
2023-03-13 01:48:41 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,8 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  applyFilterNoBackground(reddify);
 
-  
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +32,51 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter() {
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString); filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0];
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var currentColor = image[i][j];
+      if (currentColor === backgroundColor) {
+      }
+      else {
+        var rgbNumbers = rgbStringToArray(currentColor);
+        filterFunction(rgbNumbers); currentColor = rgbArrayToString(rgbNumbers);
+        image[i][j] = currentColor;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(num) {
+  rgbNumbers[RED] = 200;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(rgbNumbers) {
+  rgbNumbers[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue(rgbNumbers) {
+rgbNumbers[BLUE] = keepInBounds(rgbNumbers[BLUE] - 50);
+}
 
+function increaseGreenByBlue(rgbNumbers) {
+  rgbNumbers[GREEN] = keepInBounds(rgbNumbers[GREEN] + rgbNumbers[BLUE]);
+}
 
 // CHALLENGE code goes below here
